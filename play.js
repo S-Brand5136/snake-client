4818f3756c7ac2e68d28118890fc323d0a49017e
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-05 20:12:52 +0000

    add input.js and move play functions into

--- play.js
+++ play.js
@@ -1,24 +1,3 @@
 const { connect } = require("./client");
 
 connect();
-
-const setUpPlay = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = (key) => {
-  if (key === "\u0003") {
-    process.exit();
-  }
-
-  console.log(key);
-};
-
-setUpPlay();
